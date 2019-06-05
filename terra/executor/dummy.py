84a082cd8ae74414f3f24f8dbc7bc20f96e5fd86
Noah Johnson <noah.johnson@visionsystemsinc.com>
2019-06-05 18:02:36 -0400

    pep8

--- terra/executor/dummy.py
+++ terra/executor/dummy.py
@@ -6,41 +6,40 @@ from threading import Lock
 
 class DummyExecutor(Executor):
 
-    def __init__(self, *arg, **kwargs):
-        self._shutdown = False
-        self._shutdownLock = Lock()
+  def __init__(self, *arg, **kwargs):
+    self._shutdown = False
+    self._shutdownLock = Lock()
 
-    def submit(self, fn, *args, **kwargs):
-        with self._shutdownLock:
-            if self._shutdown:
-                raise RuntimeError('cannot schedule new futures after shutdown')
+  def submit(self, fn, *args, **kwargs):
+    with self._shutdownLock:
+      if self._shutdown:
+        raise RuntimeError('cannot schedule new futures after shutdown')
 
-            f = Future()
-            try:
-                result = fn(*args, **kwargs)
-            except BaseException as e:
-                f.set_exception(e)
-            else:
-                f.set_result(result)
+      f = Future()
+      try:
+        result = fn(*args, **kwargs)
+      except BaseException as e:
+        f.set_exception(e)
+      else:
+        f.set_result(result)
 
-            return f
+      return f
 
-    def shutdown(self, wait=True):
-        with self._shutdownLock:
-            self._shutdown = True
+  def shutdown(self, wait=True):
+    with self._shutdownLock:
+      self._shutdown = True
 
 
 if __name__ == '__main__':
 
-    def fnc(err):
-        if err:
-            raise Exception("test")
-        else:
-            return "ok"
+  def fnc(err):
+    if err:
+      raise Exception("test")
+    else:
+      return "ok"
 
-    ex = DummyExecutor()
-    print(ex.submit(fnc, True))
-    print(ex.submit(fnc, False))
-    ex.shutdown()
-    ex.submit(fnc, True)  # raises exception
-
+  ex = DummyExecutor()
+  print(ex.submit(fnc, True))
+  print(ex.submit(fnc, False))
+  ex.shutdown()
+  ex.submit(fnc, True)  # raises exception
