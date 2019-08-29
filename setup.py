f5afb6089e23b32346aa5b7b2eb3dab678c8e967
Andy Neff <andy@visionsystemsinc.com>
2019-08-29 17:54:14 -0400

    Hacks to make windows happy
    Signed-off-by: Andy Neff <andy@visionsystemsinc.com>

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
 
+extra_requires = {
+  'docker': ['docker-compose'],
+  'celery': ["celery[redis]", "flower"]
+}
+
 setup(name="terra",
       packages=["terra"],
       description="Terra",
+      extra_requires=extra_requires,
       install_requires=[
         "jstyleson",
-        "docker-compose",
-        "envcontext",
-        "celery[redis]",
-        "flower"
+        "envcontext"
       ]
 )
