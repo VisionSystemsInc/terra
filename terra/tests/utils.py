be80e9482325eb21428243440daace834333dcdc
Andy Neff <andy@visionsystemsinc.com>
2020-05-20 11:38:04 -0400

    Fixing tests executor dummy
    Signed-off-by: Andy Neff <andy@visionsystemsinc.com>

--- terra/tests/utils.py
+++ terra/tests/utils.py
@@ -1,5 +1,27 @@
+import os
+from unittest import mock
+
 from vsi.test.utils import (
   TestCase, make_traceback
 )
 
+from terra import settings
+
+
 __all__ = ["TestCase", "make_traceback"]
+
+
+class TestSettingsUnconfiguredCase(TestCase):
+  def setUp(self):
+    # Useful for tests that set this
+    self.patches.append(mock.patch.dict(os.environ,
+                                        {'TERRA_SETTINGS_FILE': ""}))
+    # Use settings
+    self.patches.append(mock.patch.object(settings, '_wrapped', None))
+    super().setUp()
+
+
+class TestSettingsConfiguredCase(TestSettingsUnconfiguredCase):
+  def setUp(self):
+    super().setUp()
+    settings.configure({})
