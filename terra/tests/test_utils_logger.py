9a89602c54cebeb1637545191473f3da24e940ed
Andy Neff <andy@visionsystemsinc.com>
2024-01-18 13:46:14 -0500

    Pep8
    Signed-off-by: Andy Neff <andy@visionsystemsinc.com>

--- terra/tests/test_utils_logger.py
+++ terra/tests/test_utils_logger.py
@@ -1,14 +1,10 @@
 from unittest import mock
-import pathlib
 import os
-import ntpath
-import posixpath
 
-from .utils import TestCase, TestLoggerConfigureCase, TestSettingsConfigureCase
-# from .test_compute_utils import TestComputeUtilsCase
+from .utils import TestLoggerConfigureCase, TestSettingsConfigureCase
 from terra.utils.logger import log_terra_version
 from terra import settings
-import terra.utils.logger
+
 
 class TestLogger(TestLoggerConfigureCase, TestSettingsConfigureCase):
   def setUp(self):
@@ -19,7 +15,8 @@ class TestLogger(TestLoggerConfigureCase, TestSettingsConfigureCase):
   @mock.patch('terra.utils.logger.Popen')
   def test_logger_git_call(self, mock_popen):
     # Test normal working case
-    communicate = mock_popen.return_value.communicate.return_value = [mock.MagicMock()]
+    communicate = [mock.MagicMock()]
+    mock_popen.return_value.communicate.return_value = communicate
     decode = communicate[0].strip.return_value.decode
     decode.return_value = 'blah-2-g7654321'
     with self.assertLogs() as cm:
@@ -39,19 +36,22 @@ class TestLogger(TestLoggerConfigureCase, TestSettingsConfigureCase):
       log_terra_version(None, None, 'Test App', 'TERRA_BLAH')
     self.assertIn('Terra Test App version: Unknown', cm.output[0])
 
-  @mock.patch.dict(os.environ, {'TERRA_BLAH_IMAGE_COMMIT': "blah-2-g1234567",
-                                'TERRA_BLAH_DEPLOY_COMMIT': "blah-2-g1234567-dirty"})
+  @mock.patch.dict(os.environ,
+                   {'TERRA_BLAH_IMAGE_COMMIT': "blah-2-g1234567",
+                    'TERRA_BLAH_DEPLOY_COMMIT': "blah-2-g1234567-dirty"})
   def test_logger(self):
     # The Deploy case, the main controller
     with self.assertLogs() as cm:
       log_terra_version(None, None, 'Test App', 'TERRA_BLAH')
-    self.assertIn('Terra Test App Deploy version: blah-2-g1234567-dirty', cm.output[0])
+    self.assertIn('Terra Test App Deploy version: blah-2-g1234567-dirty',
+                  cm.output[0])
 
     # The runner should always use the Image commit
     settings.terra.zone = 'runner'
     with self.assertLogs() as cm:
       log_terra_version(None, None, 'Test App', 'TERRA_BLAH')
-    self.assertIn('Terra Test App Runner version: blah-2-g1234567', cm.output[0])
+    self.assertIn('Terra Test App Runner version: blah-2-g1234567',
+                  cm.output[0])
 
     # Other zones should not be spamming the logs
     settings.terra.zone = 'task'
