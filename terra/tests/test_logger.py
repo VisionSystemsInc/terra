8adc68375e5da337ed8958779a5bda263b6516ec
Andy Neff <andy@visionsystemsinc.com>
2024-04-24 09:29:23 -0400

    Docs
    Signed-off-by: Andy Neff <andy@visionsystemsinc.com>

--- terra/tests/test_logger.py
+++ terra/tests/test_logger.py
@@ -51,7 +51,7 @@ class TestLogger(TestLoggerConfigureCase):
   def test_port_0(self):
     settings.configure({'logging': {'server': {'port': 0}},
                         'processing_dir': self.temp_dir.name})
-    self.assertEqual(settings.logging.server.port,  67890)
+    self.assertEqual(settings.logging.server.port, 67890)
 
   def test_temp_file_cleanup(self):
     tmp_file = self._logs.tmp_file.name
